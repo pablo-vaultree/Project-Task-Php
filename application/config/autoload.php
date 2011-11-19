0f01a8b57a79c049c228acc686747644e4750b69
Pablo Ruan <ruanmyv@gmail.com>
2011-11-19 15:23:06 -0200

    Ajustando autload dos models

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -3,22 +3,8 @@
 $autoload['packages'] = array();
 $autoload['libraries'] = array('database');
 $autoload['helper'] = array('url', 'assets_helper', 'form', 'html');
-
 $autoload['config'] = array();
-
 $autoload['language'] = array();
-
-
-/*
-| -------------------------------------------------------------------
-|  Auto-load Models
-| -------------------------------------------------------------------
-| Prototype:
-|
-|	$autoload['model'] = array('model1', 'model2');
-|
-*/
-
 $autoload['model'] = array('usuario_model', 'projeto_model', 'tarefa_model');
 
 
